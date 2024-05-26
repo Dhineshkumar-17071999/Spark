f0341425867386b6b1cdd950a7531d6bdd6db789
Dhinesh-Timesmed <dhineshk.p@timesmed.com>
2024-05-26 21:08:35 +0530

    spark session created

--- DataPipeline/data_pipeline.py
+++ DataPipeline/data_pipeline.py
@@ -1,11 +1,20 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+
 import ingest
 import transform
 import persist
 
 class Pipeline:
+    def create_spark_session(self):
+        self.spark = SparkSession.builder\
+            .appName("my first spark app")\
+                .enableHiveSupport().getOrCreate()
+
     def run_pipeline(self):
         print("running pipeline")
-        ingest_process = ingest.Ingestion()
+        ingest_process = ingest.Ingestion(self.spark)
         ingest_process.ingest_data()
         transform_process = transform.Transform()
         transform_process.transform_data()
@@ -14,4 +23,5 @@ class Pipeline:
         
 if __name__ == "__main__":
     pipeline = Pipeline()
+    pipeline.create_spark_session()
     pipeline.run_pipeline()
--- DataPipeline/ingest.py
+++ DataPipeline/ingest.py
@@ -1,3 +1,13 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+
 class Ingestion():
+    def __init__(self,spark):
+        self.spark = spark
+
     def ingest_data(self):
         print("Ingesting")
+        my_list = [1,2,3]
+        df = self.spark.createDataFrame(my_list, IntegerType())
+        df.show()
