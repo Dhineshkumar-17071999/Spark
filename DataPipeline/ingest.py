070bc3d7a3fc16feb5c07f72170b69e6f25fa4aa
Dhinesh-Timesmed <dhineshk.p@timesmed.com>
2024-05-27 08:34:35 +0530

    read csv added

--- DataPipeline/ingest.py
+++ DataPipeline/ingest.py
@@ -8,6 +8,14 @@ class Ingestion():
 
     def ingest_data(self):
         print("Ingesting")
-        my_list = [1,2,3]
-        df = self.spark.createDataFrame(my_list, IntegerType())
-        df.show()
+        # my_list = [1,2,3]
+        # df = self.spark.createDataFrame(my_list, IntegerType())
+        # df.show()
+        custome_df = self.spark.read.csv("retailstore.csv", header = True)
+        custome_df.show()
+        custome_df.describe().show()
+        custome_df.select('Country').show()
+        custome_df.groupBy("Country").count().show()
+        custome_df.filter("Salary > 3000").show()
+        custome_df.groupBy("gender").agg({"salary":"avg","age":"max"}).show()
+        custome_df.orderBy("Salary").show()
